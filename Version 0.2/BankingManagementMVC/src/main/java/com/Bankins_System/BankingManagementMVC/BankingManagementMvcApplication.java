9cbfa8216aa09db504d58903d26bea01b6bc7bae
MD. Mainul Hasan <mdmainulhasan520@gmail.com>
2024-11-07 22:22:08 +0600

    Required pakages install

---
+++ Version 0.2/BankingManagementMVC/src/main/java/com/Bankins_System/BankingManagementMVC/BankingManagementMvcApplication.java
@@ -0,0 +1,13 @@
+package com.Bankins_System.BankingManagementMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingManagementMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingManagementMvcApplication.class, args);
+	}
+
+}
---
+++ Version 0.2/BankingManagementMVC/src/test/java/com/Bankins_System/BankingManagementMVC/BankingManagementMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bankins_System.BankingManagementMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingManagementMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
